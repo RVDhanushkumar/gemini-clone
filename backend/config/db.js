adf7f737aed0f840f26df9fb80da58138fe5ecaa
RVDhanushkumar <rapetidhanushkumar13@gmail.com>
2025-05-29 18:51:39 +0530

    DB

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/geminiDB", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.DB);
     console.log("MongoDB Connected");
   } catch (err) {
     console.error("MongoDB Connection Failed:", err.message);
